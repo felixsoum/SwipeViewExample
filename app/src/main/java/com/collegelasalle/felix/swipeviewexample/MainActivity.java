e875b25ce4d21b368680b438e0d07861e1f22c73
Unknown <felix@felixsoum.com>
2018-05-21 22:30:34 -0400

    Add project

---
+++ app/src/main/java/com/collegelasalle/felix/swipeviewexample/MainActivity.java
@@ -0,0 +1,19 @@
+package com.collegelasalle.felix.swipeviewexample;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SwipeAdapter swipeAdapter = new SwipeAdapter(getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.pager);
+        viewPager.setAdapter(swipeAdapter);
+
+    }
+}
